e74df88fb974257ed2c744bb7b4101467d1339b5
wyattmatt <wyatt.honny06@gmail.com>
2026-04-07 23:14:51 +0800

    add admin panel "views_backend/" and update backend routes

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,40 @@ Route::get('/contact', function () {
 Route::get('/single-blog', function () {
     return view('views_frontend.single-blog');
 })->name('single-blog');
+
+
+
+// Backend Routes
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('views_backend.dashboard');
+    })->name('dashboard');
+
+    Route::get('/inventory', function () {
+        return view('views_backend.inventory');
+    })->name('inventory');
+
+    Route::get('/create-product', function () {
+        return view('views_backend.create-product');
+    })->name('create-product');
+
+    Route::get('/reports', function () {
+        return view('views_backend.reports');
+    })->name('reports');
+
+    Route::get('/docs', function () {
+        return view('views_backend.docs');
+    })->name('docs');
+
+    Route::get('/404', function () {
+        return view('views_backend.error-404');
+    })->name('error-404');
+
+    Route::get('/signin', function () {
+        return view('views_backend.signin');
+    })->name('signin');
+
+    Route::get('/signup', function () {
+        return view('views_backend.signup');
+    })->name('signup');
+});
