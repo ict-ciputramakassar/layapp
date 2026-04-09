a47335dac7bd78299893fd573b9b2518af0ea895
wyattmatt <wyatt.honny06@gmail.com>
2026-04-10 07:44:31 +0800

    changes made for not conflict

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UploadController;
+use App\Http\Controllers\TeamLeaderController;
+use App\Http\Controllers\AuthController;
 
 // Frontend Routes
 Route::get('/', function () {
